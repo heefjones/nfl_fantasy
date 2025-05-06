a405cdcefd03ee1cf4b3f7d587fc24aa023da9f8
heefjones <jimmyhj9@gmail.com>
2025-05-06 02:08:10 -0500

    added correct team column to html

--- helper.py
+++ helper.py
@@ -36,6 +36,40 @@ TEAM_COLORS = {'GNB': '#203731', 'CHI': '#0B162A', 'DET': '#0076B6', 'MIN': '#4F
     'BUF': '#00338D', 'NWE': '#002244', 'MIA': '#008E97', 'NYJ': '#125740', 
     'CIN': '#FB4F14', 'PIT': '#FFB612', 'BAL': '#241773', 'CLE': '#311D00',
     'TEN': '#0C2340', 'JAX': '#006778', 'HOU': '#002244', 'IND': '#003594'}
+TEAM_NAMES = {
+    'Arizona Cardinals': 'ARI',
+    'Atlanta Falcons': 'ATL',
+    'Baltimore Ravens': 'BAL',
+    'Buffalo Bills': 'BUF',
+    'Carolina Panthers': 'CAR',
+    'Chicago Bears': 'CHI',
+    'Cincinnati Bengals': 'CIN',
+    'Cleveland Browns': 'CLE',
+    'Dallas Cowboys': 'DAL',
+    'Denver Broncos': 'DEN',
+    'Detroit Lions': 'DET',
+    'Green Bay Packers': 'GB',
+    'Houston Texans': 'HOU',
+    'Indianapolis Colts': 'IND',
+    'Jacksonville Jaguars': 'JAX',
+    'Kansas City Chiefs': 'KC',
+    'Las Vegas Raiders': 'LV',
+    'Los Angeles Chargers': 'LAC',
+    'Los Angeles Rams': 'LAR',
+    'Miami Dolphins': 'MIA',
+    'Minnesota Vikings': 'MIN',
+    'New England Patriots': 'NE',
+    'New Orleans Saints': 'NO',
+    'New York Giants': 'NYG',
+    'New York Jets': 'NYJ',
+    'Philadelphia Eagles': 'PHI',
+    'Pittsburgh Steelers': 'PIT',
+    'San Francisco 49ers': 'SF',
+    'Seattle Seahawks': 'SEA',
+    'Tampa Bay Buccaneers': 'TB',
+    'Tennessee Titans': 'TEN',
+    'Washington Commanders': 'WAS'}
+
 
 # numpy seed
 SEED = 9
@@ -1053,6 +1087,9 @@ def clean_rankings(rankings):
 
     # remove "." and " Jr" from player col
     rankings['player'] = rankings['player'].str.replace('.', '', regex=False).str.replace(' Jr', '', regex=False)
+
+    # map teamName col using TEAM_NAMES
+    rankings['teamName'] = rankings['teamName'].map(TEAM_NAMES)
 
     return rankings.sort_values(by='adp_rank_2025', ascending=True).reset_index(drop=True)
 
